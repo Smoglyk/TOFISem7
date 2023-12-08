90b2dbdf99e4d76b3e1f11ed906bd9af98fb7274
Dimam <Dimaman12@yandex.by>
2023-12-08 18:09:23 +0300

    add fix

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from dotenv import load_dotenv
+from database.database import DB
+from settings import settings
+from supabase import create_client, Client
+
+load_dotenv()
+
+
+db:Client = create_clinet(settings.database_url, settings.databse_key)
 
 app = FastAPI()
+
+@app.middleware("http")
+async def add_session(request: Request, call_next):
+    request.session =  supabase.auth.get_session()
+    response = await call_next(request)
+
 
 
 @app.get("/")
--- settings.py
+++ settings.py
@@ -1,6 +1,9 @@
-# import os
-# from supabase import create_client, Client
-#
-# url: str = os.environ.get("SUPABASE_URL")
-# key: str = os.environ.get("SUPABASE_KEY")
-# supabase: Client = create_client(url, key)
+from pydantic import BaseSettings
+from database.database import DB
+
+
+class Settings(BaseSettings):
+    database_url: str = os.environ.get("SUPABASE_URL")
+    databse_key: str = os.environ.get("SUPABASE_KEY")
+
+settings = Settings()
